7ac3b0b7a51dac9cf7b1fecf65ff418ca15e982d
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-07-24 23:21:02 -0400

    Support a default delta value to select a tab.

--- src/_dg_quick_tabs.js
+++ src/_dg_quick_tabs.js
@@ -1,2 +1,4 @@
 var dg_quick_tabs = new dg.Module(); // Create the module.
 dg.modules.dg_quick_tabs = dg_quick_tabs; // Attach it to DrupalGap.
+
+dg._quick_tabs = {}; // Holds onto active quick tabs objects.
--- src/widgets/widget.quick-tabs.js
+++ src/widgets/widget.quick-tabs.js
@@ -1,9 +1,13 @@
-dg._quick_tabs = {};
-
 /**
- * @TODO move this to a contrib module.
+ * Renders a quick tabs widget.
  * @param variables
- *  _tabs {Object} This should be identical to what you would pass into a custom item_list widget.
+ *  _tabs {Object} An item_list widget that will be used to render the tabs. @see Item List Widget
+ *  _delta {Number} Optional, the index of the tab to set as the default. Defaults to 0.
+ *  _panes {Function} A function to handle what content to display, it will be passed two arguments:
+ *      %param quickTabs {Object} Contains all data about the quick tabs widget.
+ *      %param delta {Number} The index of the tab that was clicked.
+ *      %return {Promise|String} Return a Promise and resolve it with an html string, or just return an html string.
+ * @return {String} The html string of a rendered quick tabs widget.
  */
 dg.theme_quick_tabs = function(variables) {
 
@@ -40,7 +44,10 @@ dg.theme_quick_tabs = function(variables) {
     html += '<div class="quick-tabs-pane hidden" data-quick-tabs-pane="' + i + '"></div>';
   }
 
+  // After the html is returned and rendered on the page...
   setTimeout(function() {
+
+    // Grab the tabs and attach a click listener to each.
     var listItems = document.querySelectorAll('#' + id + ' .quick-tabs li');
     for (var i = 0; i < listItems.length; i++) {
       var item = listItems[i];
@@ -50,21 +57,25 @@ dg.theme_quick_tabs = function(variables) {
         var _item = event.target;
 
         // The clicked element won't necessarily be the <li> housing the item, so we traverse up the DOM from the
-        // target clicked element in search of the <li> around it. When we find the list item, quickly grab a reference
-        // to the list itself.
+        // target clicked element in search of the <li> around it. When we find the list item, grab a reference to the
+        // list itself.
         var _list = null;
         var _listItem = null;
         if (_item.tagName != 'LI') {
           for (var j = 0; j < event.path.length; j++) {
             var path = event.path[j];
             if (path.tagName == 'LI') {
-              _listItem = path;
               _list = event.path[j+1];
+              _listItem = path;
               break;
             }
           }
         }
-        else { _list = event.path[1]; }
+        else {
+          _list = event.path[1];
+          _listItem = event.path[0];
+        }
+
         if (!_listItem || !_list) { return; } // Bail out if we couldn't find the list item or the list.
 
         // Figure out the delta of the clicked item.
@@ -72,19 +83,21 @@ dg.theme_quick_tabs = function(variables) {
         //var pane = quickTabs._panes[delta];
         //if (!pane) { return; }
 
-        // Hide all its sibling panes.
+        // Hide all sibling panes.
         for (var k = 0; k < _list.childNodes.length; k++) {
           if (k == delta) { continue; }
           var otherPane = document.querySelector('#' + id + ' div[data-quick-tabs-pane="' + k + '"]');
           dg.addClass(otherPane, 'hidden');
         }
 
-        // Grab the corresponding pane for the delta.
+        // Grab the corresponding pane for the clicked tab's delta.
         var paneDiv = document.querySelector('#' + id + ' div[data-quick-tabs-pane="' + delta + '"]');
 
         // Only render the pane if it is empty (aka render it once the first time), then no matter what un hide it.
         if (paneDiv.innerHTML == '') {
 
+          // Call the quick tabs panes handler, and inject its html into the correct pane. If the developer returns a
+          // Promise, wait until it is resolved to inject its html into the pane.
           var pane = quickTabs._panes(quickTabs, delta);
           if (jDrupal.isPromise(pane)) {
             pane.then(function(_html) {
@@ -94,10 +107,17 @@ dg.theme_quick_tabs = function(variables) {
           else { paneDiv.innerHTML = dg.render(pane, true); }
 
         }
+
+        // Show the active pane.
         dg.removeClass(paneDiv, 'hidden');
 
       });
     }
+
+    // If there is a default delta, simulate a click on it to set the default tab.
+    if (typeof variables._delta === 'undefined') { variables._delta = 0; }
+    var defaultDelta = variables._delta;
+    if (listItems[defaultDelta]) { listItems[defaultDelta].click(); }
 
   }, 1);
 
