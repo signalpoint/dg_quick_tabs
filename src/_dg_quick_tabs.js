da17cf939614765cbc2939c3cb708216a59e722b
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-03-06 14:08:12 -0500

    Use dg.createModule() syntax instead of legacy syntax.

--- src/_dg_quick_tabs.js
+++ src/_dg_quick_tabs.js
@@ -1,4 +1,3 @@
-var dg_quick_tabs = new dg.Module(); // Create the module.
-dg.modules.dg_quick_tabs = dg_quick_tabs; // Attach it to DrupalGap.
+dg.createModule('dg_quick_tabs');
 
 dg._quick_tabs = {}; // Holds onto active quick tabs objects.
