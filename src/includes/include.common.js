b2480b8c41c77508e68fdaee9d4222add66b3dce
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-03-14 17:48:20 -0400

    Add removePagerEvents() and addPagerEvents().

--- src/includes/include.common.js
+++ src/includes/include.common.js
@@ -10,3 +10,13 @@ dg_quick_tabs.save = function(id, variables) {
 dg_quick_tabs.clear = function() {
   dg._quick_tabs = {};
 };
+
+dg_quick_tabs.removePagerEvents = function(pagerId) {
+  var pager = dg_pager.load(pagerId);
+  if (pager) { pager.removeEvents(); }
+};
+
+dg_quick_tabs.addPagerEvents = function(pagerId) {
+  var pager = dg_pager.load(pagerId);
+  if (pager && !pager.eventsAdded()) { pager.addEvents(); }
+};
