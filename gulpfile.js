c32e9c3142b831215e711528297158fa2ca4c4f0
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2021-05-13 15:07:53 -0400

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,59 @@
+var makeBinary = true;
+
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
     gp_uglify = require('gulp-uglify'),
-    minifyCSS = require('gulp-minify-css');
+    gp_minify_css = require('gulp-minify-css');
 
-var dgQuickTabsModuleSrc = [
+var jsSrc = [
   './src/_dg_quick_tabs.js',
   './src/includes/include.*.js',
   './src/widgets/widget.*.js'
 ];
 
-var dgQuickTabsCssSrc = ['./css/*.css'];
+// Minify JavaScript
+function minifyJs() {
+  console.log('compressing dg_quick_tabs.js...');
+  var js = gulp.src(jsSrc)
+    .pipe(gp_concat('dg_quick_tabs.js'))
+    .pipe(gulp.dest('./'));
+  if (makeBinary) {
+    console.log('compressing dg_quick_tabs.min.js...');
+    return js.pipe(gp_rename('dg_quick_tabs.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./'));
+  }
+  return js;
+}
+gulp.task(minifyJs);
 
-// Task to build the dg_curtain.min.js file.
-gulp.task('minifyJS', function(){
-  return gulp.src(dgQuickTabsModuleSrc)
-      .pipe(gp_concat('concat.js'))
-      .pipe(gulp.dest(''))
-      .pipe(gp_rename('dg_quick_tabs.min.js'))
-      .pipe(gp_uglify())
-      .pipe(gulp.dest(''));
-});
+var cssSrc = [
+  './css/*.css'
+];
 
-// Task to build the dg_curtain.min.css file.
-gulp.task('minifyCSS', function(){
-  gulp.src(dgQuickTabsCssSrc)
-      .pipe(gp_concat('concat.css'))
-      .pipe(gulp.dest(''))
-      .pipe(gp_rename('dg_quick_tabs.min.css'))
-      .pipe(minifyCSS())
-      .pipe(gulp.dest(''));
-});
+// Minify CSS
+function minifyCss() {
+  console.log('compressing dg_quick_tabs.css...');
+  var css = gulp.src(cssSrc)
+    .pipe(gp_concat('dg_quick_tabs.css'))
+    .pipe(gulp.dest('./'));
+  if (makeBinary) {
+    console.log('compressing dg_quick_tabs.min.css...');
+    return css.pipe(gp_rename('dg_quick_tabs.min.css'))
+    .pipe(gp_minify_css())
+    .pipe(gulp.dest('./'));
+  }
+  return css;
+}
+gulp.task(minifyCss);
 
-gulp.task('default', function () {
-  watch(dgQuickTabsModuleSrc, function(event) { gulp.start('minifyJS') });
-  watch(dgQuickTabsCssSrc, function(event) { gulp.start('minifyCSS') });
-  gulp.start(['minifyJS', 'minifyCSS']);
+gulp.task('default', function(done) {
+
+  gulp.watch(jsSrc, gulp.series('minifyJs'));
+  gulp.watch(cssSrc, gulp.series('minifyCss'));
+
+  done();
+
 });
